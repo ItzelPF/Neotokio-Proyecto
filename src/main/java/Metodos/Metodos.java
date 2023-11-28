477e490f7c79b68dac9032b4d26d7e5d72d5ca4f
itzym <ItzelPF>
2023-11-27 19:52:27 -0600

    v2

---
+++ src/main/java/Metodos/Metodos.java
@@ -0,0 +1,10 @@
+
+package Metodos;
+
+/**
+ *
+ * @author itzym
+ */
+public class Metodos {
+    //Aqui iran los metodos usados en este modulo
+}
--- src/main/java/Principal/Main.java
+++ src/main/java/Principal/Main.java
@@ -1,6 +1,6 @@
 package Principal;
 public class Main {
     public static void main(String[] args) {
-        //
+        //Aqui se pondrá correr lo de reservaciones
     }
 }
